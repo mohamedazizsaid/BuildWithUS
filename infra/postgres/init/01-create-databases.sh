df9f3d4fc07a14cf4dc9b70a5797d02ab0d50a15
aziz <saidazizz132@gmail.com>
2026-09-09 15:35:43 +0100

    fix

--- infra/postgres/init/01-create-databases.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-for db in auth_db templates_db; do
-  psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE DATABASE $db;
-    GRANT ALL PRIVILEGES ON DATABASE $db TO $POSTGRES_USER;
-EOSQL
-done
